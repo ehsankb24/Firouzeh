518029d0683cb3223e9d4f5f10642f10c9300f11
ehsan zarghalami <e.zarghalami@hamrahcard.ir>
2025-05-30 20:45:20 +0330

    implement article screens rout

---
+++ news/src/main/java/kb24/ehsan/news/navigation/ArticleDetailRout.kt
@@ -0,0 +1,6 @@
+package kb24.ehsan.news.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ArticleDetailRout(val articleId: Long)
---
+++ news/src/main/java/kb24/ehsan/news/navigation/ArticlesRout.kt
@@ -0,0 +1,6 @@
+package kb24.ehsan.news.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object ArticlesRout
